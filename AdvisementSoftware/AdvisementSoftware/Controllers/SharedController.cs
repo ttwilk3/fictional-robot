a59a425de476446e21db86ed6444d8e65b1ad2e0
lohrun <lohrun@gmail.com>
2017-10-08 20:27:38 -0400

    Added UserProfile Query

--- AdvisementSoftware/AdvisementSoftware/Controllers/SharedController.cs
+++ AdvisementSoftware/AdvisementSoftware/Controllers/SharedController.cs
@@ -68,5 +68,82 @@ namespace AdvisementSoftware.Controllers
                 return null;
             }
         }
+
+        public class UserProfile
+        {
+            public string CourseID { get; set; }
+            public string CourseName { get; set; }
+            public int CreditHours { get; set; }
+            public string Comment { get; set; }
+        }
+
+        public string getProfile()
+        {
+            string userName = User.Identity.Name;
+            DataTable table = new DataTable();
+            DataTable table2 = new DataTable();
+            string findID = "SELECT Id FROM ASPNETUSERS WHERE Email = '" + userName + "'";
+            string findProfile = "SELECT USERPROFILE.CourseID, COURSES.CourseName, COURSES.CreditHours, USERPROFILE.Comment FROM ASPNETUSERS INNER JOIN USERPROFILE ON USERPROFILE.UserID = '";
+            try
+            {
+                string path = System.IO.Directory.GetCurrentDirectory();
+
+                string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(findID, connectionString);
+
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+
+                table.Locale = System.Globalization.CultureInfo.InvariantCulture;
+
+                dataAdapter.Fill(table);
+
+                string userID = table.Rows[0][0].ToString();
+
+
+
+                findProfile += userID + "' INNER JOIN COURSES ON USERPROFILE.CourseID = COURSES.CourseID";
+
+                dataAdapter = new SqlDataAdapter(findProfile, connectionString);
+
+                commandBuilder = new SqlCommandBuilder(dataAdapter);
+
+                dataAdapter.Fill(table2);
+
+                List<UserProfile> ups = new List<UserProfile>();
+                foreach (DataRow r in table2.Rows)
+                {
+                    UserProfile up = new UserProfile();
+                    foreach (DataColumn c in table2.Columns)
+                    {
+                        //CourseID
+                        //CourseName
+                        //CreditHours
+                        //Comment
+                        if (c.ColumnName.Equals("CourseID"))
+                            up.CourseID = r[c.ColumnName.ToString()].ToString();
+                        else if (c.ColumnName.Equals("CourseName"))
+                            up.CourseName = r[c.ColumnName.ToString()].ToString();
+                        else if (c.ColumnName.Equals("CreditHours"))
+                        {
+                            int temp = 0;
+                            Int32.TryParse(r[c.ColumnName.ToString()].ToString(), out temp);
+                            up.CreditHours = temp;
+                        }
+                        else if (c.ColumnName.Equals("Comment"))
+                            up.Comment = r[c.ColumnName.ToString()].ToString();
+
+                    }
+                    ups.Add(up);
+                }
+
+                string json = JsonConvert.SerializeObject(ups);
+                return json;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
     }
 }
